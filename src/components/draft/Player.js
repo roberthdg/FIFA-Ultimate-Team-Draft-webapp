691e2f63618ba92c27b6aabe8a23ab5534948b81
Roberth Gómez <roberthdgg@gmail.com>
2020-02-17 21:23:18 -0400

    Added formation data

--- src/components/draft/Player.js
+++ src/components/draft/Player.js
@@ -8,6 +8,10 @@ function Player(props)  {
       }
       <div id="player">
         {props.position.toUpperCase()} ({props.number})
+        <svg height="200" width="200">
+          <line x1="0" y1="100" x2="100" y2="200" stroke="green" stroke-width="4"/>
+          <line x1="200" y1="100" x2="100" y2="200" stroke="red" stroke-width="4"/>
+        </svg>
       </div>
     </>
   )
--- src/formationsData.js
+++ src/formationsData.js
@@ -1,8 +1,74 @@
 
 const formation = [
+                //4-4-2
                 {'gk': 0, 'rb': 1, 'rcb':2, 'lcb':3, 'lb':4, 'rm':5, 'rcm':6, 'lcm':7, 'lm':8, 'rst':9, 'lst':10}, 
-                {'gk':0, 'rb': 1, 'rcb':2, 'lcb':3, 'lb':4, 'rcm':5, 'cm':6, 'lcm':7, 'rw':8, 'st':9, 'lw':10}
+                //4-3-3
+                {'gk':0, 'rb': 1, 'rcb':2, 'lcb':3, 'lb':4, 'rcm':5, 'cam':6, 'lcm':7, 'rw':8, 'st':9, 'lw':10}
             ]
+
+
+const formation442 = [
+    { 
+        'position': 'GK',
+        'links' : ['RCB', 'LCB'],
+        'chemistryLines' : [{'RCB': []}, {'LCB': []}]
+    },
+    //Defenders
+    { 
+        'position': 'RB',
+        'links' : ['RCB', 'RM'],
+        'chemistryLines' : [{'RCB': []}, {'RM': []}]
+    },
+    { 
+        'position': 'RCB',
+        'links' : ['GK', 'LCB', 'RCM', 'RB'],
+        'chemistryLines' : [{'RCM': []}, {'LCB': []}]
+    },
+    { 
+        'position': 'LCB',
+        'links' : ['GK', 'RCB', 'LCM', 'LB'],
+        'chemistryLines' : [{'LCM': []}, {'LB': []}]
+    },
+    { 
+        'position': 'LB',
+        'links' : ['LCB', 'LM'],
+        'chemistryLines' : [{'LM': []}]
+    },
+    //Midfielders
+    { 
+        'position': 'RM',
+        'links' : ['RST', 'RCM', 'RB'],
+        'chemistryLines' : [{'RCM': []}, {'RST': []}]
+    },
+    { 
+        'position': 'RCM',
+        'links' : ['RCB', 'RM', 'LCM', 'RST'],
+        'chemistryLines' : [{'LCM': []}, {'RST': []}]
+    },
+    { 
+        'position': 'LCM',
+        'links' : ['LCB', 'LM', 'RCM', 'LST'],
+        'chemistryLines' : [{'LM': []}, {'LST': []}]
+    },
+    { 
+        'position': 'LM',
+        'links' : ['LST', 'LC', 'LB'],
+        'chemistryLines' : [{'LST': []}]
+    },
+    // Forwards
+    { 
+        'position': 'RST',
+        'links' : ['RM', 'RCM', 'LST'],
+        'chemistryLines' : [{'LST': []}]
+    },
+    { 
+        'position': 'LST',
+        'links' : ['LM', 'LCM', 'RST'],
+        'chemistryLines' : []
+    }    
+]
+    
+
 
 
 const formationLinks = [
@@ -38,3 +104,5 @@ const formationLinks = [
 exports.formation = formation
 
 exports.formationLinks = formationLinks
+
+
